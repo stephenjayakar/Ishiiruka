cc02ec0ce47a5a23cad7214ac3f71a3abf6013d2
Nikhil Narayana <nikhil.narayana@live.com>
2020-12-02 18:37:11 -0800

    add build-mac.sh

--- build-mac.sh
+++ build-mac.sh
@@ -7,7 +7,7 @@ DESKTOP_APP_URL="https://github.com/project-slippi/slippi-desktop-app"
 DESKTOP_APP_SYS_PATH="./slippi-desktop-app/app/dolphin-dev/overwrite/Sys"
 
 DATA_SYS_PATH="./Data/Sys/"
-BINARY_PATH="./build/Binaries/Slippi Dolphin.app/Contents/Resources/"
+BINARY_PATH="./build/Binaries/Slippi\ Dolphin.app/Contents/Resources/"
 
 # Build type
 if [ "$1" == "playback" ]
@@ -18,29 +18,27 @@ else
         echo "Using Netplay build config"
 fi
 
-if [[ -z "${CERTIFICATE_MACOS_APPLICATION}" ]]
+if [[ -z "$MACOS_CERT_PASSWD" ]]
     then
-        echo "Building without code signing"
-else
         echo "Building with code signing"
-        CMAKE_FLAGS+=' -DMACOS_CODE_SIGNING="ON"'
+        CMAKE_FLAGS+='-DMACOS_CODE_SIGNING="ON"'
+else
+        echo "Building without code signing"
 fi
 
 # Move into the build directory, run CMake, and compile the project
 mkdir -p build
 pushd build
-cmake ${CMAKE_FLAGS} ..
-make -j7
+cmake ${CMAKE_FLAGS} ../
+make -j$(nproc)
 popd
 
 # Copy the Sys folder in
-echo "Copying Sys files into the bundle"
-cp -Rfn "${DATA_SYS_PATH}" "${BINARY_PATH}"
+cp -Rfn ${DATA_SYS_PATH} ${BINARY_PATH}
 
 # Copy playback specific codes if needed
 if [ "$1" == "playback" ]
     then
-        echo "Copying playback codes into the bundle"
         if [ -d "slippi-desktop-app" ]
 			then
 				pushd slippi-desktop-app
@@ -52,5 +50,5 @@ if [ "$1" == "playback" ]
 		fi
         
         rm -rf "${BINARY_PATH}/Sys/GameSettings"
-        cp -r "${DESKTOP_APP_SYS_PATH}" "${BINARY_PATH}"
+        cp -r ${DESKTOP_APP_SYS_PATH} ${BINARY_PATH}
 fi
