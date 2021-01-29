6605a2e7312351e691b91e652faee94e355d7471
Jas Laferriere <Fizzi36@gmail.com>
2021-01-29 14:07:03 -0800

    attempt to move codes into heap region

--- Source/Core/Core/GeckoCode.cpp
+++ Source/Core/Core/GeckoCode.cpp
@@ -150,13 +150,22 @@ static bool InstallCodeHandler()
 	u32 codelist_base_address = INSTALLER_BASE_ADDRESS + (u32)data.length() - 8;
 	u32 codelist_end_address = INSTALLER_END_ADDRESS;
 
+	u32 melee_heap_start = 0x80bd5c40;
+	u32 melee_code_size_max = 0x50000;
+	u32 melee_fixed_inject_ofst = 0x34;
+
+	// Move Gecko code handler into the heap region
 	if(SConfig::GetInstance().m_gameType == GAMETYPE_MELEE_NTSC)
 	{
-		// Move Gecko code handler to the tournament mode region
-		codelist_base_address = 0x801910E0;
-		codelist_end_address = 0x8019AF4C;
-		PowerPC::HostWrite_U32(0x3DE08019, 0x80001f58);
-		PowerPC::HostWrite_U32(0x61EF10E0, 0x80001f5C);
+		// Start of the heap plus offset for our size overwrite patch
+		codelist_base_address = melee_heap_start + melee_fixed_inject_ofst;
+
+		// Set max size we allow for gecko codes, how big can this be before game doesn't have enough heap?
+		codelist_end_address = codelist_base_address + melee_code_size_max;
+
+		// Overwrite codehandler to point to our custom code list
+		PowerPC::HostWrite_U32(0x3DE080bd, 0x80001f58); // lis r15, 0x80BD
+		PowerPC::HostWrite_U32(0x61EF5c40 + melee_fixed_inject_ofst, 0x80001f5C); // ori r3, r3, 0x5C4C
 
 		// Here we are replacing a line in the codehandler with a blr.
 		// The reason for this is that this is the section of the codehandler
@@ -165,6 +174,8 @@ static bool InstallCodeHandler()
 		// the game to loop infinitely in EXISync.
 		PowerPC::HostWrite_U32(0x4E800020, 0x80001D6C);
 	}
+
+	NOTICE_LOG(ACTIONREPLAY, "Gecko Bounds: %X - %X", codelist_base_address, codelist_end_address);
 
 	// Write a magic value to 'gameid' (codehandleronly does not actually read this).
 	PowerPC::HostWrite_U32(0xd01f1bad, INSTALLER_BASE_ADDRESS);
@@ -202,8 +213,44 @@ static bool InstallCodeHandler()
 		}
 	}
 
+	// Write terminator
 	PowerPC::HostWrite_U32(0xff000000, codelist_base_address + 8 + i);
 	PowerPC::HostWrite_U32(0x00000000, codelist_base_address + 12 + i);
+
+	if (SConfig::GetInstance().m_gameType == GAMETYPE_MELEE_NTSC)
+	{
+		// Overwrite the end address to only store the code list and nothing more
+		codelist_end_address = codelist_base_address + 16 + i;
+
+		// Overwrite some game logic that determines where to start the heap. This causes the game
+		// to not touch the section of code where we wrote the gct
+		PowerPC::HostWrite_U32(0x3C600000 | (codelist_end_address >> 16), melee_heap_start); // lis r3, 0xXXXX (top half of new start of heap)
+		PowerPC::HostWrite_U32(0x60630000 | (codelist_end_address & 0x0000FFFF), melee_heap_start + 0x4); // lis r3, 0xXXXX (bottom half of new start of heap)
+		PowerPC::HostWrite_U32(0x4B43FE88, melee_heap_start + 0x8); // b -0xBC017C # 80bd5c4C -> 80015ad0, branch back
+
+		// Branch to the logic defined above when game is calculating heap start
+		PowerPC::HostWrite_U32(0x48BC0174, 0x80015ACC);  // b 0xBC0174 # 80015acc -> 80bd5c40
+		PowerPC::ppcState.iCache.Invalidate(0x80015ACC); // invalidate this address
+
+		// Overwrite more game logic that clears memory to prevent the game from clearing the memory we just set
+		PowerPC::HostWrite_U32(0x3CC080BD, melee_heap_start + 0xC); // lis r6, 0x80bd
+		PowerPC::HostWrite_U32(0x60C65C40, melee_heap_start + 0x10); // ori r6, r6, 0x5c40
+		PowerPC::HostWrite_U32(0x7CA33050, melee_heap_start + 0x14); // sub r5, r6, r3
+		PowerPC::HostWrite_U32(0x4B42D4A9, melee_heap_start + 0x18); // bl -0xBD2B54 # memset. 80BD5C58 -> 80003100
+		PowerPC::HostWrite_U32(0x3C600000 | (codelist_end_address >> 16), melee_heap_start + 0x1C); // lis r3, 0x80bd
+		PowerPC::HostWrite_U32(0x60630000 | (codelist_end_address & 0x0000FFFF), melee_heap_start + 0x20); // ori r3, r3, 0x5c4C
+		PowerPC::HostWrite_U32(0x38800000, melee_heap_start + 0x24); // li r4, 0
+		PowerPC::HostWrite_U32(0x7CA3F050, melee_heap_start + 0x28); // sub r5, r30, r3
+		PowerPC::HostWrite_U32(0x4B42D495, melee_heap_start + 0x2C); // bl -0xBD2B68 # memset. 80BD5C6C -> 80003100
+		PowerPC::HostWrite_U32(0x4B76D290, melee_heap_start + 0x30); // b -0xBC017C # branch back. 80BD5C70 -> 80342f00
+
+		// Branch to the logic defined above when game is clearing memory
+		PowerPC::HostWrite_U32(0x48892D50, 0x80342efc);  // b 0xBC0174 # 80342efc -> 80bd5c4c
+		PowerPC::ppcState.iCache.Invalidate(0x80342efc); // invalidate this address
+
+		// Overwrite the base address such that our overwritten logic will get invalidated
+		codelist_base_address = melee_heap_start;
+	}
 
 	// Turn on codes
 	PowerPC::HostWrite_U8(1, INSTALLER_BASE_ADDRESS + 7);
